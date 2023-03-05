447c139533e5d1ce9e109f6197554a7020c716b4
Daniel <iapyzov8@gmail.com>
2023-03-05 15:08:21 +0300

    1 commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const arr = [1,2,3,4,5,6,7]
+const arrFilter = arr.filter((el) => el === 2 || e === 7);
+console.log(arrFilter);
+
+
+// const arr = [1,2,3,4,5,6,7]
+// const arrReduce = arr.reduce((el, i) => el + i )
+// console.log(arrReduce);
+
+// const arr = [1,2,3,4,5,6,7]
+// const arrFind = arr.find((el) => el > 3)
+// console.log(arrFind);
+
+
+// const arr = [1,2,3,4,5,6,7]
+// const arrFindIndex = arr.findIndex((el) => el > 3 )
+// console.log(arrFindIndex);
+
+// const arr = ['sanjar','Ilim','Artur','Adil']
+// const arrIndexOf = arr.indexOf('Adil')
+// console.log(arrIndexOf);
+
+// const arr = ['sanjar','Ilim','Artur','Adil']
+// const arrSome = arr.some(el => el === 'Adil')
+// console.log(arrSome);
+
+
+// const arr = [1,2,3,4,5,6,7,8,20]
+// const arrSome = arr.some(el => el === 20)
+// console.log(arrSome);
+
+// let arr = ['a','b','c','d','e']
+// let arr2 = arr.slice(2,4)
+// console.log(arr2);
+
+
+
+// let arr = [55, 1, 34, 2, 3, 4, 5, 6, 7,  20]
+// let arr2 = (ar1,ar2) => ar1 - ar2
+// arr.sort(arr2)
+// console.log(arr);
+
+
+// const arr = []
+// const arrConcat = arr.concat('a','a','a')
+// console.log(arrConcat);
+
+
+// let arr = [5, 2, -1]
+// let arr2 = (ar1,ar2) => ar1 - ar2
+// arr.sort(arr2)
+// console.log(arr);
+
+// function renge() {
+//     // let arr = [5, 2, -1]
+// let arr2 = (ar1,ar2) => ar1 - ar2
+// // arr.sort(arr2)
+// // console.log(arr);
+// renge.sort
+// }
+
+// renge(1,4,56,7)
+
+// console.log(renge);
+
+
+
